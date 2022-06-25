0ae8d84e6f469a85caf214ae91e02081385dfeeb
Philippe GUEMKAM SIMO <philippechampion58@gmail.com>
2022-06-25 09:37:51 +0200

    End of kafka configuration

--- freeland-collab-live/src/main/java/tech/zerofiltre/freeland/collab/infra/providers/notification/ServiceContractKafkaNotificationProvider.java
+++ freeland-collab-live/src/main/java/tech/zerofiltre/freeland/collab/infra/providers/notification/ServiceContractKafkaNotificationProvider.java
@@ -17,6 +17,35 @@ import tech.zerofiltre.freeland.collab.infra.providers.notification.model.*;
 @RequiredArgsConstructor
 public class ServiceContractKafkaNotificationProvider implements ServiceContractNotificationProvider {
 
+    private final KafkaTemplate<String, KafkaServiceContractEvent> kafkaTemplate;
 
+    @Value("${service-contract.topic}")
+    private String topic;
+
+    private final KafkaServiceContractEventMapper mapper;
+
+
+    @Override
+    public void notify(ServiceContractEvent serviceContractEvent) {
+        KafkaServiceContractEvent payload = mapper.toKafka(serviceContractEvent);
+        ListenableFuture<SendResult<String, KafkaServiceContractEvent>> future = kafkaTemplate
+                .send(topic, payload);
+        future.addCallback(new ListenableFutureCallback<>() {
+
+            @Override
+            public void onSuccess(SendResult<String, KafkaServiceContractEvent> result) {
+                log.info("Message [{}] delivered with offset {}",
+                        payload.toString(),
+                        result.getRecordMetadata().offset());
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                log.warn("Unable to deliver message [{}]. {}",
+                        payload.toString(),
+                        ex.getMessage());
+            }
+        });
+    }
 }
 
